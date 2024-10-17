4b57dc726b0550b3434dc19cdab32549900371c4
Lucy Harris <lucyalisonharris@gmail.com>
2024-10-17 14:50:38 +0200

    yaml file with topics for multiple connections

--- src/ros_publish.py
+++ src/ros_publish.py
@@ -7,8 +7,8 @@ import rospy
 from rospy_message_converter import json_message_converter
 from utils import import_msg_type
 
-class ros_publish():
 
+class ros_publish:
     def __init__(self):
 
         # initialize node
@@ -16,44 +16,54 @@ class ros_publish():
         rospy.on_shutdown(self.shutdown)
 
         # Retrieve parameters from launch file
-        bootstrap_server = rospy.get_param("~bootstrap_server", "localhost:9092")
+        bootstrap_server = rospy.get_param(
+            "~bootstrap_server", "localhost:9092"
+        )
         self.ros_topic = rospy.get_param("~ros_topic", "test")
         self.kafka_topic = rospy.get_param("~kafka_topic", "test")
         self.msg_type = rospy.get_param("~msg_type", "std_msgs/String")
 
         # Create kafka consumer
-        self.consumer = KafkaConsumer(self.kafka_topic,
-                                    bootstrap_servers=bootstrap_server,
-                                    value_deserializer=lambda m: json.loads(m.decode('ascii')),
-                                    auto_offset_reset='latest',
-                                    consumer_timeout_ms=5000)
+        self.consumer = KafkaConsumer(
+            self.kafka_topic,
+            bootstrap_servers=bootstrap_server,
+            value_deserializer=lambda m: json.loads(m.decode("ascii")),
+            auto_offset_reset="latest",
+            consumer_timeout_ms=5000,
+        )
 
         # Import msg type
         msg_func = import_msg_type(self.msg_type)
-        
-        # Subscribe to ROS topic of interest
-        self.publisher = rospy.Publisher(self.ros_topic, msg_func, queue_size=10)
-        rospy.logwarn("Using {} MSGs from KAFKA: {} -> ROS: {}".format(self.msg_type, self.kafka_topic,self.ros_topic))
-        
 
+        # Subscribe to ROS topic of interest
+        self.publisher = rospy.Publisher(
+            self.ros_topic, msg_func, queue_size=10
+        )
+        rospy.logwarn(
+            "Using {} MSGs from KAFKA: {} -> ROS: {}".format(
+                self.msg_type, self.kafka_topic, self.ros_topic
+            )
+        )
 
     def run(self):
- 
+
         while not rospy.is_shutdown():
             for msg in self.consumer:
                 # Convert Kafka message to JSON string
                 json_str = json.dumps(msg.value)
                 # Convert JSON to ROS message
-                ros_msg = json_message_converter.convert_json_to_ros_message(self.msg_type, json_str)
+                ros_msg = json_message_converter.convert_json_to_ros_message(
+                    self.msg_type, json_str
+                )
                 # Publish to ROS topic
                 self.publisher.publish(ros_msg)
                 rospy.logwarn("Received MSG: {}".format(json_str))
-        
-        rospy.spin()
 
+        rospy.spin()
 
     def shutdown(self):
         rospy.loginfo("Shutting down")
+
 
 if __name__ == "__main__":
 
--- src/utils.py
+++ src/utils.py
@@ -1,74 +1,148 @@
 #!/usr/bin/env python
 
+import rospy
+import yaml
+
 """
 For custom msg types, need to build pkg in workspace and then add here
 """
 
-def import_msg_type(msg_type):
 
-    # Adding a new msg type is as easy as including an import and updating the variable 
+def load_yaml_to_dict(yaml_file: str, robot_name: str = "UGV") -> dict:
+    """
+    takes yaml file and converts to dict which includes topic names & msg types
+
+    :param yaml_file (str): path to yaml file
+    :param robot_name(str, optional): first line of yaml file, default -> 'UGV' 
+    :return topic_dict (dict): dictionary mapping topics & msg types
+
+    example: topic_list.yaml
+
+    robot_name:
+        - msg_type: "std_msgs/String"
+          ros_topic: "/string"
+          kafka_topic: "string"
+        - msg_type: "geometry_msgs/Pose"
+          ros_topic: "/pose"
+          kafka_topic: "pose"
+
+    topic_dict: {
+        'std_msgs/String': {'/string', 'string'},
+        'geometry_msgs/Pose' : {'/pose', 'pose'}
+    }
+    """
+    with open(yaml_file, "r") as file:
+        yaml_data = yaml.safe_load(file)
+
+    topics_dict = {}
+    for topic in yaml_data[robot_name]:
+        msg_type = topic["msg_type"]
+        ros_topic = topic["ros_topic"]
+        kafka_topic = topic["kafka_topic"]
+
+        topics_dict[msg_type] = {
+            "ros_topic": ros_topic,
+            "kafka_topic": kafka_topic,
+        }
+
+    return topics_dict
+
+
+def import_msg_type(msg_type: str):
+    """
+    takes a ros msg_type and dynamically imports the msg type and returns it
+    
+    :params msg_type (str): the string identifier for the ROS msg type
+    :return subscriber_type (class): the corresponding ROS msg class
+    :raises ValueError: if msg_type is not found
+    """
     if msg_type == "std_msgs/String":
         from std_msgs.msg import String
+
         subscriber_msg = String
-    if msg_type == "std_msgs/Bool":
+    elif msg_type == "std_msgs/Bool":
         from std_msgs.msg import Bool
+
         subscriber_msg = Bool
-    if msg_type == "std_msgs/Empty":
+    elif msg_type == "std_msgs/Empty":
         from std_msgs.msg import Empty
-        subscriber_msg = Bool
+
+        subscriber_msg = Empty
     elif msg_type == "geometry_msgs/Twist":
         from geometry_msgs.msg import Twist
+
         subscriber_msg = Twist
     elif msg_type == "geometry_msgs/Pose":
         from geometry_msgs.msg import Pose
+
         subscriber_msg = Pose
     elif msg_type == "geometry_msgs/PoseArray":
         from geometry_msgs.msg import PoseArray
+
         subscriber_msg = PoseArray
     elif msg_type == "geometry_msgs/PoseStamped":
         from geometry_msgs.msg import PoseStamped
+
         subscriber_msg = PoseStamped
     elif msg_type == "geometry_msgs/PoseWithCovariance":
         from geometry_msgs.msg import PoseWithCovariance
+
         subscriber_msg = PoseWithCovariance
     elif msg_type == "geometry_msgs/PoseWithCovarianceStamped":
         from geometry_msgs.msg import PoseWithCovarianceStamped
+
         subscriber_msg = PoseWithCovarianceStamped
     elif msg_type == "geometry_msgs/Vector3":
         from geometry_msgs.msg import Vector3
+
         subscriber_msg = Vector3
-    elif msg_type == "sensors_msgs/Image":
+    elif msg_type == "sensor_msgs/Image":
         from sensor_msgs.msg import Image
+
         subscriber_msg = Image
     elif msg_type == "sensor_msgs/LaserScan":
         from sensor_msgs.msg import LaserScan
+
         subscriber_msg = LaserScan
     elif msg_type == "sensor_msgs/BatteryState":
         from sensor_msgs.msg import BatteryState
+
         subscriber_msg = BatteryState
     elif msg_type == "sensor_msgs/Imu":
         from sensor_msgs.msg import Imu
+
         subscriber_msg = Imu
     elif msg_type == "sensor_msgs/PointCloud2":
         from sensor_msgs.msg import PointCloud2
+
         subscriber_msg = PointCloud2
     elif msg_type == "sensor_msgs/JointState":
         from sensor_msgs.msg import JointState
+
         subscriber_msg = JointState
     elif msg_type == "sensor_msgs/NavSatFix":
         from sensor_msgs.msg import NavSatFix
+
         subscriber_msg = NavSatFix
     elif msg_type == "nav_msgs/Odometry":
         from nav_msgs.msg import Odometry
+
         subscriber_msg = Odometry
     elif msg_type == "nav_msgs/OccupancyGrid":
         from nav_msgs.msg import OccupancyGrid
+
         subscriber_msg = OccupancyGrid
     elif msg_type == "actionlib_msgs/GoalStatus":
         from actionlib_msgs.msg import GoalStatus
+
         subscriber_msg = GoalStatus
+    elif msg_type == "tf2_msgs/TFMessage":
+        from tf2_msgs.msg import TFMessage
+
+        subscriber_msg = TFMessage
     else:
-        raise ValueError("MSG NOT SUPPORTED: Only String/Twist/Image are currently supported. \
-                          Please add imports to utils.py for specific msg type.")
-    
+        raise ValueError(
+            f'MSG "{msg_type}" IS NOT SUPPORTED \nPlease add imports to utils.py for specific msg type.'
+        )
+
     return subscriber_msg
