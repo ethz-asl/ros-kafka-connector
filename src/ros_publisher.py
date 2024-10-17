5bc51174ec030b08f8c5048a4239cc4cdc13f1da
Lucy Harris <lucyalisonharris@gmail.com>
2024-10-17 15:13:39 +0200

    added ros publisher to use yaml

--- src/ros_publisher.py
+++ src/ros_publisher.py
@@ -9,22 +9,10 @@ from rospy_message_converter import json_message_converter
 import utils
 
 
-class KafkaPublisher:
-    # TODO need to make kafka topics!!
-    """
-    takes a yaml file with:
-    - ros msg types
-    - ros topic names
-    - kafka topic names
-
-    subscribes to the ros topic, and converts to json format
-    publishes json msg to the kafka topic
-
-    """
-
+class ROSPublisher:
     def __init__(self):
 
-        rospy.init_node("kafka_publisher")
+        rospy.init_node("ros_publisher")
         rospy.on_shutdown(self.shutdown)
 
         self.load_parameters()
@@ -33,26 +21,29 @@ class KafkaPublisher:
             pkg.get_path("ros_kafka_connector") + "/config/" + self._filename
         )
 
-        topics_dict = utils.load_yaml_to_dict(yaml_file, self._robot_name) 
+        topics_dict = utils.load_yaml_to_dict(yaml_file, self._robot_name)
 
-        # start kafka producer
-        # self.producer = KafkaProducer(
-        #     bootstrap_servers=self._bootstrap_server,
-        #     value_serializer=lambda m: json.dumps(m).encode('ascii')
-        # )
-
+        self.consumers = []
+        self.publishers = []
         for msg_type, topics in topics_dict.items():
             ros_topic = topics["ros_topic"]
             kafka_topic = topics["kafka_topic"]
-            msg_class = utils.import_msg_type(msg_type)
-            rospy.Subscriber(
-                ros_topic,
-                msg_class,
-                lambda msg: self.callback(msg, kafka_topic),
+
+            consumer = KafkaConsumer(
+                kafka_topic,
+                bootstrap_servers=self._bootstrap_server,
+                value_deserializer=lambda m: json.loads(m.decode("ascii")),
+                auto_offset_reset="latest",
+                consumer_timeout_ms=5000,
             )
+            msg_class = utils.import_msg_type(msg_type)
+            publisher = rospy.Publisher(ros_topic, msg_class, queue_size=10)
             rospy.loginfo(
                 f"Using {msg_type} from ROS: {ros_topic} -> KAFKA: {kafka_topic}"
             )
+
+            self.consumers.append(consumer)
+            self.publishers.append(publisher)
 
     def load_parameters(self) -> None:
         self._filename = rospy.get_param("~topics_filename", "topics.yaml")
@@ -61,22 +52,20 @@ class KafkaPublisher:
         )
         self._update_rate = float(rospy.get_param("~update_rate", "10.0"))
         self._robot_name = rospy.get_param("~robot_name", "UGV")
-
-    def callback(self, msg, kafka_topic: str) -> None:
-        """
-        takes msg from ros, converts to json, publishes to kafka
         
-        :param msg: ros msg from subscriber 
-        :param kafka_topic (str): kafka topic name
-
-        """
-        json_str = json_message_converter.convert_ros_message_to_json(msg)
-        # self.producer.send(kafka_topic, json_str)
 
     def run(self):
-        rate = rospy.Rate(self._update_rate)
+
         while not rospy.is_shutdown():
-            rate.sleep()
+            for consumer, publisher in zip(self.consumers, self.publishers):
+                for msg in consumer:
+                    json_str = json.dump(msg.value)
+                    ros_msg = json_message_converter.convert_json_to_ros_message(
+                        publisher.type, json_str
+                    )
+                    publisher.publish(ros_msg)
+
+        rospy.spin()
 
     def shutdown(self):
         rospy.loginfo("Shutting down")
@@ -85,7 +74,7 @@ class KafkaPublisher:
 if __name__ == "__main__":
 
     try:
-        node = KafkaPublisher()
+        node = ROSPublisher()
         node.run()
     except rospy.ROSInterruptException:
         pass
